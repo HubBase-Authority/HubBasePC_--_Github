da261041bf7f258fc4cdae317e4a1d2b7c5620e9
Voice659 <E.R.Fahretdinov@yandex.ru>
2026-04-12 10:32:16 +0300

    Update Version Backlog.py to a8-a8.3.1

--- Version Backlog.py
+++ Version Backlog.py
@@ -1,6 +1,6 @@
-print("--- Version Backlog a8-a8.2.1 (default, Apr 11 2026) ---")
-print("V`s available are a8.0.0 - a8.2.1")
-print("Newest - a8, start - a8")
+rint("--- Version Backlog a8-a8.3.1 (default, Apr 12 2026) ---")
+print("V`s available are a8.0.0 - a8.3.1(in plans)")
+print("Newest - a8(a9 in plans), start - a8")
 D1 = input("The Main part -- ").lower()
 print("1,2,3 and so on")
 D2 = input("The addition -- ")
@@ -8,6 +8,7 @@ print("1,2,3 and so on")
 D3 = input("The fix -- ")
 print("B - for betas, R - for releases")
 D4 = input("Access to Version -- ").upper()
+print("")
 if D1 == "a8":
     if D2 == "0":
         if D3 == "0":
@@ -73,7 +74,31 @@ if D1 == "a8":
                 print("Such release does not exist or isn`t documented")
         else:
             print("Such release does not exist or isn`t documented")
+    elif D2 == "3":
+        if D3 == "0":
+            if D4 == "B":
+                print("--- HubBase a8.3.0B (default, Apr 12 2026, 9:30:29) ---")
+                print("Addition №3!(in beta)")
+                print("Changes:")
+                print("    - Added 'Programm14()'(with bugs)")
+            elif D4 == "R":
+                print("--- HubBase a8.3.0 (default, Apr 12 2026, 10:00:36) ---")
+                print("Addition №3!")
+                print("Changes:")
+                print("    - Added 'Programm14()' fully")
+            else:
+                print("Such release does not exist or isn`t documented")
+        elif D3 == "1":
+            print("This release is not complete, it is only a plan!(Might change!)")
+            print("Bugfix №3!")
+            print("Changes:")
+            print("    - Fixed 'Programm14()' not working for Decrypt mode")
+        else:
+            print("Such release does not exist or isn`t documented")
     else:
         print("Such release does not exist or isn`t documented")
+elif D1 == "a9":
+    print("This release is not complete, it is only a plan!(Might change!)")
 else:
     print("Such release does not exist or isn`t documented")
+s = input("")
